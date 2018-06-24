ec18dd9bb75dbf4061720f27c862fa9348339635
PowerShopMCPE <37243611+PowerShop@users.noreply.github.com>
2018-06-25 06:29:44 +0700

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TrueWallet | DevExp</title>
+  </head>
+  <body>
+    <form action="wallet.php" method="post">
+      <input type="text" name="wallet" placeholder="เลขที่อ้างอิง">
+      <br>
+      <input type="text" name="member" placeholder="ชื่อสมาชิก">
+      <br>
+      <input type="submit" name="submit" value="OK">
+    </form>
+  </body>
+</html>
